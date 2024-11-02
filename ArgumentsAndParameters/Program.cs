cb354e5bacc0b3526187493c3cd48158bfa02d4f
RaigoAljas <Raigo.Aljas@tktk.ee>
2024-11-02 20:20:29 +0200

    add week7 code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Console.WriteLine("Enter someting:");
+string userInput = Console.ReadLine();
+
+
+PrintAnyWord(userInput); //userInput - argument
+
+
+
+static void PrintAnyWord(string anyString) //anyString - parameter
+{
+    anyString = anyString.ToUpper();
+    for(int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
+
+
+    
+
+
+
+
+
+
+
+
---
+++ Week7Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Are you coming or leaving? (int/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in" )
+{
+    PrintHello();
+}
+{
+    PrintGoodBye();
+}
+
+
+
+
+
+//PrintHello();
+//PrintGoodBye(); 
+
+
+
+static void PrintHello() //fuction / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
